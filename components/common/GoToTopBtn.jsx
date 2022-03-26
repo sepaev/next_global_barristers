25c724f66cbcf66e692bb15a6d33589e4ac1a085
Sepaev <sepaev@gmail.com>
2022-03-26 10:29:25 +0200

    gototop

--- components/common/GoToTopBtn.jsx
+++ components/common/GoToTopBtn.jsx
@@ -3,10 +3,17 @@ import Image from 'next/image'
 import btnGoTop from '../../images/svg/btnGoTop.svg'
 
 export function GoToTopBtn() {
+  const gototop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    })
+  }
+
   return (
     <section className='goTop'>
       <div className='goTop__container container'>
-        <div className='goTop__image'>
+        <div onClick={gototop} className='goTop__image'>
           <Image src={btnGoTop} alt='Go to top' className='goTop__svg' />
         </div>
       </div>
